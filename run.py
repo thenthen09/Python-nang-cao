4eb2a935bb5f6ef9138a8a2072c42ff7a2b0622f
thenthen09 <165476272+thenthen09@users.noreply.github.com>
2024-11-05 12:48:37 +0700

    Create run.py

---
+++ run.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+from tkinter import messagebox
+from test import DatabaseApp
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = DatabaseApp(root)
+    root.mainloop()
